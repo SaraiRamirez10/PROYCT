2ed319f8bc670ebf02315fa713cc8d0894f318d0
yurilopez10 <alexan2000hotmail@gmail.com>
2019-11-13 15:57:22 -0600

    Agregando a proyecto

--- app/src/main/java/com/example/proyectosara/MainActivity.java
+++ app/src/main/java/com/example/proyectosara/MainActivity.java
@@ -2,9 +2,27 @@ package com.example.proyectosara;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private static String APP_DIRECTORY = "MyPictureApp/";
+    private static String MEDIA_DIRECTORY = APP_DIRECTORY + "PictureApp";
+
+    private final int MY_PERMISSIONS = 100;
+    private final int PHOTO_CODE = 200;
+    private final int SELECT_PICTURE = 300;
+
+    private ImageView mSetImage;
+    private Button mOptionButton;
+    private RelativeLayout mRlView;
+
+    private String mPath;
+    private static final int TAKE_PICTURE = 0;
+    private Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
