65d244e7c6312a97d0719bc61eb2335fe289ccd8
christian-hdz <75574579hdz@gmail.com>
2019-11-15 11:47:34 -0600

    agg

--- app/src/main/java/com/example/proyectosara/MainActivity.java
+++ app/src/main/java/com/example/proyectosara/MainActivity.java
@@ -184,5 +184,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-}
+        if(grantResults.length == 2 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED){
+    }
+    }
 }
