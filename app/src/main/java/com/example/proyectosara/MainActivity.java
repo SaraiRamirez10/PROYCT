b963c90fd21d2958880da90f122af481b588a422
yurilopez10 <alexan2000hotmail@gmail.com>
2019-11-15 11:14:45 -0600

    agregando condiciones

--- app/src/main/java/com/example/proyectosara/MainActivity.java
+++ app/src/main/java/com/example/proyectosara/MainActivity.java
@@ -151,6 +151,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK){
+            switch (requestCode){
+
+            }
 
     }
 }
